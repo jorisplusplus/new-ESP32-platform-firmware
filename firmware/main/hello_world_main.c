9447e41f172378aa4804efeb1185be790f295a3a
Joris Witteman <joriswitteman@hotmail.com>
2020-08-01 16:50:34 +0200

    Removed delay in starting

--- firmware/main/hello_world_main.c
+++ firmware/main/hello_world_main.c
@@ -11,7 +11,7 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
-
+#include "micropython_main.h"
 
 void app_main()
 {
@@ -30,11 +30,8 @@ void app_main()
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
+    printf("Starting mp now.\n");
     fflush(stdout);
-    esp_restart();
+    start_mp();
+    
 }
