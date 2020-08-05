c3c7d5f7d95d4250931bffa3a86bb1dcee11555b
Joris Witteman <joriswitteman@hotmail.com>
2020-08-05 20:26:13 +0200

    Manifest support for builtin modules

---
+++ firmware/modules/hello_world.py
@@ -0,0 +1,9 @@
+
+def hello():    # write Fibonacci series up to n
+    print("hello")
+
+def world():   # return Fibonacci series up to n
+    print("world")
+
+def hello_world():
+    print("hello world")
